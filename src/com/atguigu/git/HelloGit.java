7e0db9175253748e23e367ca542d25c492b249e6
chiheng163 <chiheng_ltpnew@163.com>
2020-11-12 20:35:53 +0800

    回复idea

--- src/com/atguigu/git/HelloGit.java
+++ src/com/atguigu/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("初次使用Git");
 		System.out.println("你好,这里是idea也是正在使用Git操作");
+		System.out.println("好的!好的!我这边已经收到了");
 	}
 }
